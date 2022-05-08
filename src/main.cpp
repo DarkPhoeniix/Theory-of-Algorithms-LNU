0ac6188323bac3bdc1a7773a776ff8af79b55065
Maryna Vovchuk <marina.vovchuk777@gmail.com>
2022-05-08 21:18:07 +0300

    1001: implemented bubble sort algorithm in cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,28 @@
 
-#define PY_SSIZE_T_CLEAN
-#include <python3.10/Python.h>
 #include "Utility.h"
+#include "Sort.h"
 
 #include <iostream>
 #include <chrono>
 
-
-const int SIZE = 1000000;
+const int SIZE = 1000;
 
 int main(int argc, char* argv[])
 {
-    Py_Initialize();
-    PyRun_SimpleString("from time import time,ctime\n"
-                       "print('Today is',ctime(time()))\n");
-    Py_Finalize();
-//    int* array = new int[SIZE];
-//
-//    Utility::fillRandom(array, SIZE, -100, 100);
-//
-//    std::chrono::time_point start = std::chrono::high_resolution_clock::now();
-//    // TODO: sort here
-//    std::chrono::time_point end = std::chrono::high_resolution_clock::now();
-//
-//    std::cout << "Data: " << '\n'
-//        << "size: " << SIZE << '\n'
-//        << "time: " <<  std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << " ms" << std::endl;
-//
-//    delete[] array;
+    int* array = new int[SIZE];
+
+    Utility::fillRandom(array, SIZE, -100, 100);
+
+    std::chrono::time_point start = std::chrono::high_resolution_clock::now();
+    // TODO: sort here
+    Sort::bubbleSort(array, SIZE);
+    std::chrono::time_point end = std::chrono::high_resolution_clock::now();
+
+    std::cout << "Data: " << '\n'
+        << "size: " << SIZE << '\n'
+        << "time: " <<  std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << " ms" << std::endl;
+
+    delete[] array;
 
     return 0;
 }
