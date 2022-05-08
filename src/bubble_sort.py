b89c5908784f9ff816d1d7df5fea8b4d138444d6
Oleksii Datsiuk <Olmine2013@gmail.com>
2022-05-08 22:09:12 +0300

    1002:implemented bubble sort algorithm using python

---
+++ src/bubble_sort.py
@@ -0,0 +1,9 @@
+"""Implementation of bubble sort"""
+
+
+def bubble_sort(array: list[int], array_length: int) -> None:
+    """Function, which sorts an array using bubble sort algorithm """
+    for i in range(array_length):
+        for j in range(array_length):
+            if array[i] < array[j]:
+                array[i], array[j] = array[j], array[i]
