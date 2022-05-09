e3e40c630fb6b4189fd5c910b02152305047b9a8
Oleksii Datsiuk <Olmine2013@gmail.com>
2022-05-09 12:08:15 +0300

    1004:implemented counting sort algorithm by radix using python

---
+++ src/counting_sort.py
@@ -0,0 +1,31 @@
+"""Implementation of counting sort in python"""
+
+
+def counting_sort(array: list[int], radix: int):
+    """Function, which sorts an array by specified radix
+    using counting sort algorithm
+    """
+    array_length = len(array)
+
+    output = [0 for _ in range(array_length)]
+
+    count = [0 for _ in range(10)]
+
+    for i in range(0, array_length):
+        index = array[i] // radix
+        count[index % 10] += 1
+
+    for i in range(1, 10):
+        count[i] += count[i - 1]
+
+    i = array_length - 1
+    while i >= 0:
+        index = array[i] // radix
+        output[count[index % 10] - 1] = array[i]
+        count[index % 10] -= 1
+        i -= 1
+
+    i = 0
+    for i in range(0, len(array)):
+        array[i] = output[i]
+
