d732b007040befa21b5402936c7d29d5c27a69c5
Maryna Vovchuk <marina.vovchuk777@gmail.com>
2022-05-10 20:41:58 +0300

    1005: embedden Python interpreter in cpp code and tested bubble sort algorithm

--- src/main.cpp
+++ src/main.cpp
@@ -2,46 +2,32 @@
 #include "Utility.h"
 #include "Sort.h"
 
-#include <iostream>
-#include <chrono>
+#include <exception>
+#include <vector>
+#include <pybind11/embed.h>
+#include <pybind11/cast.h>
+#include <pybind11/stl.h>
 
-const int SIZE = 100000;
+const int SIZE = 10;
 
 int main(int argc, char* argv[])
 {
+    pybind11::scoped_interpreter guard{};
+    pybind11::exec(R"(
+                import sys
+                import os
+                sys.path.append(os.path.abspath(os.getcwd()))
+    )");
+
+    pybind11::module_ pyModule = pybind11::module_::import("scripts.bubble_sort");
+
     int* array = new int[SIZE];
-
     Utility::fillRandom(array, SIZE, -100, 100);
 
-    std::chrono::time_point start = std::chrono::high_resolution_clock::now();
-    Sort::bubbleSort(array, SIZE);
-    std::chrono::time_point end = std::chrono::high_resolution_clock::now();
-
-    std::cout << "Bubble sort: " << '\n'
-        << "size: " << SIZE << '\n'
-        << "time: " <<  std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << " ms" << std::endl;
-
-    Utility::fillRandom(array, SIZE, -100, 100);
-
-    start = std::chrono::high_resolution_clock::now();
-    Sort::countingSort(array, SIZE);
-    end = std::chrono::high_resolution_clock::now();
-
-    std::cout << "Counting sort: " << '\n'
-              << "size: " << SIZE << '\n'
-              << "time: " <<  std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << " ms" << std::endl;
-
-    Utility::fillRandom(array, SIZE, -100, 100);
-
-    start = std::chrono::high_resolution_clock::now();
-    Sort::radixSort(array, SIZE);
-    end = std::chrono::high_resolution_clock::now();
-
-    std::cout << "Radix sort: " << '\n'
-              << "size: " << SIZE << '\n'
-              << "time: " <<  std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << " ms" << std::endl;
+    auto pyBubbleSort = pyModule.attr("bubble_sort");
+    pybind11::list pyList = pybind11::cast(std::vector<int>(array, array + SIZE));
+    pyBubbleSort(pyList, SIZE);
 
     delete[] array;
-
     return 0;
 }
