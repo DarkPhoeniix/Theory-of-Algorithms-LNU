951bf215af042c3bd83410f955b910c789445dce
Oleksii Datsiuk <Olmine2013@gmail.com>
2022-05-10 22:03:27 +0300

    1010: Improved application class, resolved all issues

--- src/Application.cpp
+++ src/Application.cpp
@@ -1,23 +1,54 @@
 #include "Application.h"
 #include "Sort.h"
 #include "Utility.h"
+
 #include <iostream>
 #include <chrono>
 
-Application::Application(){
-    arrayGenerators["fillRandom"] = (Utility::fillRandom);
-    arrayGenerators["fillSorted"] = (Utility::fillSorted);
-    arrayGenerators["fillSortedReversed"] = (Utility::fillSortedReversed);
-    arrayGenerators["fillSortedWithSubArrays"] = (Utility::fillSortedSubArrays);
-    arrayGenerators["fillSortedWithSwaps"] = (Utility::fillSortedWithSwaps);
+namespace {
+    bool validate(int argc, char* argv[])
+    {
+        bool result = true;
+        if (argc < 4)
+        {
+            result &= 0;
+        }
+
+        int lowerBound = std::stoi(argv[1]);
+        int upperBound = std::stoi(argv[2]);
+        int step = std::stoi(argv[3]);
+
+        if (lowerBound < 0 || upperBound < 0 || step < 0)
+        {
+            result &= 0;
+        }
+        else if (lowerBound > upperBound)
+        {
+            result &= 0;
+        }
+        else if ((upperBound - lowerBound) % step != 0)
+        {
+            result &= 0;
+        }
+        return result;
+    }
+}
+
+Application::Application()
+{
+    arrayGenerators["fillRandom"] = Utility::fillRandom;
+    arrayGenerators["fillSorted"] = Utility::fillSorted;
+    arrayGenerators["fillSortedReversed"] = Utility::fillSortedReversed;
+    arrayGenerators["fillSortedWithSubArrays"] = Utility::fillSortedSubArrays;
+    arrayGenerators["fillSortedWithSwaps"] = Utility::fillSortedWithSwaps;
 
     sortingAlgorithms["bubbleSort"] = Sort::bubbleSort;
     sortingAlgorithms["radixSort"] = Sort::radixSort;
 }
 
-int Application::execute(int argc, char *argv[])
+int Application::execute(int argc, char* argv[])
 {
-    if (argc < 4)
+    if (!validate(argc, argv))
     {
         return 1;
     }
@@ -26,26 +57,12 @@ int Application::execute(int argc, char *argv[])
     int upperBound = std::stoi(argv[2]);
     int step = std::stoi(argv[3]);
 
-    if (lowerBound < 0 || upperBound < 0 || step < 0)
-    {
-        return 1;
-    } else if (lowerBound > upperBound)
-    {
-        return 1;
-    }else if ((upperBound - lowerBound) % step != 0)
-    {
-        return 1;
-    }
-
     for (int size = lowerBound; size <= upperBound; size += step)
     {
-        std::cout << "SORTING ARRAYS OF SIZE " << size << '\n'
-                  << "_______________________________" << '\n';
         for (auto arrayGen : arrayGenerators)
         {
             int* arrayToSort = new int[size];
             arrayGen.second(arrayToSort, size, -(size/2), (size/2));
-
             for(auto sortingAlgorithm : sortingAlgorithms)
             {
                 std::chrono::time_point start = std::chrono::high_resolution_clock::now();
@@ -54,7 +71,7 @@ int Application::execute(int argc, char *argv[])
 
                 std::cout << sortingAlgorithm.first << "using" << arrayGen.first << '\n'
                           << "size: " << size << '\n'
-                          << "time: " <<  std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << " ms" << std::endl;
+                          << "time: " << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << " ms" << std::endl;
             }
         }
     }
--- src/Application.h
+++ src/Application.h
@@ -1,7 +1,7 @@
 #ifndef TASK_01_APPLICATION_H
 #define TASK_01_APPLICATION_H
 
-#include <iostream>
+#include <string>
 #include <map>
 #include <functional>
 
@@ -9,12 +9,13 @@ class Application{
 public:
     Application();
     int execute(int argc, char* argv[]);
+
 private:
     using SortingAlgorithm = std::function<void(int*, size_t)>;
-    using ArrayGenerator = std::function<void(int*, int, int, int)>;
+    using ArrayGenerator = std::function<void(int*, size_t, int, int)>;
 
     std::map<std::string, ArrayGenerator> arrayGenerators;
     std::map<std::string, SortingAlgorithm> sortingAlgorithms;
 };
 
-#endif
+#endif //TASK_01_APPLICATION_H
