9c4ca35d4e6fcdbd1b880d190bdd305a6c31d64e
Maryna Vovchuk <marina.vovchuk777@gmail.com>
2022-05-11 08:12:11 +0300

    1010: fixed memory leak. Made some minor improvements

--- src/Application.cpp
+++ src/Application.cpp
@@ -10,6 +10,7 @@ namespace
     bool validate(int argc, char* argv[])
     {
         bool result = true;
+
         if (argc < 4)
         {
             result &= 0;
@@ -61,19 +62,22 @@ int Application::execute(int argc, char* argv[])
 
     for (int size = lowerBound; size <= upperBound; size += step)
     {
-        for (auto arrayGen : arrayGenerators)
+        for (const auto& sortingAlgorithm : sortingAlgorithms)
         {
-            int* arrayToSort = new int[size];
-            arrayGen.second(arrayToSort, size, -(size/2), (size/2));
-            for(auto sortingAlgorithm : sortingAlgorithms)
+            for(const auto& arrayGen : arrayGenerators)
             {
+                int* arrayToSort = new int[size];
+                arrayGen.second(arrayToSort, size, -(size/2), (size/2));
+
                 std::chrono::time_point start = std::chrono::high_resolution_clock::now();
                 sortingAlgorithm.second(arrayToSort, size);
                 std::chrono::time_point end = std::chrono::high_resolution_clock::now();
 
                 std::cout << sortingAlgorithm.first << "using" << arrayGen.first << '\n'
                           << "size: " << size << '\n'
-                          << "time: " << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << " ms" << std::endl;
+                          << "time: " << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << " ms" << std::endl << std::endl;
+
+                delete[] arrayToSort;
             }
         }
     }
