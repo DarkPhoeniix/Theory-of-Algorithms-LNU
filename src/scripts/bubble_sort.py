ff6c592da0b3b9495e8fa408e40f503f73316f2c
Oleksii Datsiuk <Olmine2013@gmail.com>
2022-05-19 14:47:22 +0300

    1012: Determined time complexity of sorting algorithms

--- src/scripts/bubble_sort.py
+++ src/scripts/bubble_sort.py
@@ -1,10 +1,12 @@
 """Implementation of bubble sort"""
 
 
-def bubble_sort(array: list[int]) -> None:
+def bubble_sort(array: list[int]) -> None: #1 + n*(n*8)
     """Function, which sorts an array using bubble sort algorithm """
-    size = len(array)
-    for i in range(size):
-        for j in range(size):
-            if array[i] < array[j]:
-                array[i], array[j] = array[j], array[i]
+    size = len(array) # 1
+    for i in range(size): # n
+
+        for j in range(size): # n
+
+            if array[i] < array[j]: # 1+1+1
+                array[i], array[j] = array[j], array[i] # 1+1+1+1+1
